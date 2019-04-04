d51c804379715feebc893d034ef709808618c4df
radicalrooster <rdehaan86@hotmail.nl>
2019-04-04 15:41:46 +0200

    Fishtank
     A game where i portray the temperature of cities with fish controlled by api's

---
+++ Project3_2D_Koersen (1)/Assets/Scripts/Api.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Api : MonoBehaviour
+{
+    private const string API_KEY = "bd3c3ba5e9a485c081d34e8084941e7d";
+    private const string URL = "api.openweathermap.org/data/2.5/weather?id=2759794&units=metric&appid="+API_KEY;
+    private const string URL_2 = "api.openweathermap.org/data/2.5/weather?id=6058560&mode=+xml&appid="+API_KEY;
+    
+    public Text responseText;
+    public Text responseText2;
+    
+    public IEnumerator Request()
+    {
+      /*UnityWebRequest www = UnityWebRequest.Get(URL);
+        yield return www.Send();
+
+        if (www.isError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+        /*WWW request = new WWW(URL);
+        StartCoroutine(onResponse(request));
+        
+        WWW request2 = new WWW(URL_2);
+        StartCoroutine(onResponse2(request2));#1#*/
+        
+
+
+        WWW www = new WWW(URL);
+
+        while (!www.isDone)
+
+            yield return null;
+
+        if (string.IsNullOrEmpty (www.error)) {
+
+            Debug.Log (www.text);
+
+        } else
+
+            Debug.Log (www.error);
+    }
+    
+    
+    private IEnumerator onResponse(WWW req)
+    {
+        yield return req;
+
+        responseText.text = req.text;
+    }
+
+    private IEnumerator onResponse2(WWW request2)
+    {
+        yield return request2;
+
+        responseText2.text = request2.text;
+    }
+
+    //private IEnumerator onRequest(temp)
+    
+}
---
+++ Project3_2D_Koersen (1)/Assets/Scripts/Fish1_Api.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+using UnityEditor.IMGUI.Controls;
+
+public class Fish1_Api : MonoBehaviour
+{
+    public const string URL = "api.openweathermap.org/data/2.5/weather?id=2759794&units=metric&appid="+API_KEY; // verander locatie
+    private const string API_KEY = "9d555c538ff33e5450ae1575097d7b97";
+    public Text responseText;
+
+    public string retrievedCountry;
+    public string retrievedCity;
+    public int conditionID;
+    public string conditionName;
+    public static float floatTemp;
+    public float height;
+    
+    public Slider netherlandsSlider;
+    public Text netCountry;
+    public Text netCity;
+    public Text netTemperature;
+    public Text netCondition;
+  
+    
+    public bool Right;
+    public Animator Fish1;
+    public GameObject FISH1;
+    private Vector3 fishPos;
+    
+    IEnumerator Start()
+    {
+        WWW request = new WWW(URL);
+        yield return request;
+        if (request.error == null || request.error == "Error")
+        {
+            var N = JSON.Parse(request.text);
+
+            retrievedCountry = N["sys"]["country"].Value;
+            retrievedCity = N["name"].Value;
+
+            string temp = N["main"]["temp"].Value;
+
+            float.TryParse(temp, out floatTemp);
+            //float finalTemp = Mathf.Round(tempTemp / 100);
+            Debug.Log(temp);
+            Debug.Log(floatTemp);
+
+            int.TryParse(N["weather"][0]["id"].Value, out conditionID);
+            conditionName = N["weather"][0]["description"].Value;
+
+            netCountry.text = "" + retrievedCountry;
+            netCity.text = "City:" + retrievedCity;
+            netTemperature.text = "Temperature:" + temp + " C";
+            netCondition.text = "Condition:" + conditionName;
+
+            //netherlandsSlider.value = finalTemp;
+        }
+        else
+        {
+            Debug.Log("WWW error: " + request.error);
+
+        }
+    }
+    
+    // Start is called before the first frame update
+    void awake()
+    {
+         transform.Translate(Time.deltaTime, 0, 0);
+         Fish1 = GetComponent<Animator>();
+         FISH1 = GameObject.FindGameObjectWithTag("Fish");
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        height = floatTemp;
+       
+         if (transform.position.x <= -7.5)
+         {
+             Right = true;
+             Fish1.SetBool("Left", false);
+             Fish1.SetBool("Right", true);
+         }
+
+         if (transform.position.x >= 7.5)
+         {    
+             Right = false;
+             Fish1.SetBool("Right", false);
+             Fish1.SetBool("Left", true);
+         }
+
+         if (Right)
+         {
+             transform.Translate(Time.deltaTime, 0, 0);
+         }
+        
+         if (Right == false)
+         {
+            transform.Translate(-Time.deltaTime, 0, 0);
+         }
+
+         if (transform.position.x > -7.5 && transform.position.x < 7.5)
+         {
+            //Debug.Log("SWIM 2");
+         }
+    }
+
+    public void Onbutton()
+    {
+        FISH1.transform.position = fishPos;
+        fishPos.y = height/1000;   
+    }
+}
