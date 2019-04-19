f243b1c461cdc7860e6f4505a3ecee9e216bfbe6
radicalrooster <rdehaan86@hotmail.nl>
2019-04-19 12:15:58 +0200

    Project game added
    I added the a api project to my github

--- Project3_2D_Koersen (1)/Assets/Scripts/Fish1_Api.cs
+++ Project3_2D_Koersen (1)/Assets/Scripts/Fish1_Api.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using SimpleJSON;
-using UnityEditor.IMGUI.Controls;
+//using UnityEditor.IMGUI.Controls;
 
 public class Fish1_Api : MonoBehaviour
 {
